15a0292772d892ceb47db7dbd81d886499c73f16
LAB11_USER ON WS25 <L11X09W25@LabHCI.net>
2026-09-18 09:51:20 +0800

    Add extra VIP threshold bonus - olivar.eden

--- orders.js
+++ orders.js
@@ -22,7 +22,7 @@ function calculateLoyaltyPoints(order) {
     points = points * 1.5;
   }
 
-  return Math.round(points);
+  return Math.round(points) + (order.total > 200 ? 2 : 0);
 }
 
 module.exports = { createOrder, applyDiscount, cancelOrder, calculateLoyaltyPoints };
